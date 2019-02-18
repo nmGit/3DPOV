069185b281453d299305ae639e112e78181d5cf5
Noah Meltzer <nmeltzer@wisc.edu>
2019-02-18 13:25:38 -0600

    uart echo

--- Firmware/Test_Projects/Test1/3D_POV_Main_Board/source/communication/DebugUart.cpp
+++ Firmware/Test_Projects/Test1/3D_POV_Main_Board/source/communication/DebugUart.cpp
@@ -1,7 +1,13 @@
+#include "debugUart.h"
 #include <stdio.h>
-
+#include <ti/drivers/UARTMSP432.h>
 /* Override printf */
 int dbgprint()
 {
+    // Loop forever echoing
+    while (1) {
+        UART_read(uart, &input, 1);
+        UART_write(uart, &input, 1);
+    }
 
 }
