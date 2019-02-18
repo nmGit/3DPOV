6de588af159268c1ab142d93e1f39a59e764262e
Noah Meltzer <nmeltzer@wisc.edu>
2019-02-18 13:25:23 -0600

    uart header

---
+++ Firmware/Test_Projects/Test1/3D_POV_Main_Board/source/communication/include/DebugUart.h
@@ -0,0 +1,15 @@
+/*
+ * DebugUart.h
+ *
+ *  Created on: Feb 17, 2019
+ *      Author: Noah
+ */
+
+#ifndef SOURCE_COMMUNICATION_INCLUDE_DEBUGUART_H_
+#define SOURCE_COMMUNICATION_INCLUDE_DEBUGUART_H_
+
+int dbgprint();
+
+
+
+#endif /* SOURCE_COMMUNICATION_INCLUDE_DEBUGUART_H_ */
