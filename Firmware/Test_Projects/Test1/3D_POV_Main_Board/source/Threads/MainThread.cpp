10e9098d4ebe13fb0b7fccd5ec31c3eaa86a366e
Noah Meltzer <nmeltzer@wisc.edu>
2019-02-24 17:25:13 -0600

    Start organizing main thread

---
+++ Firmware/Test_Projects/Test1/3D_POV_Main_Board/source/Threads/MainThread.cpp
@@ -0,0 +1,17 @@
+/*
+ * MainThread.cpp
+ *
+ *  Created on: Feb 24, 2019
+ *      Author: Noah
+ */
+
+#include "PAL/PALThread.h"
+
+class MainThread : public PALThread
+{
+public:
+protected:
+private:
+};
+
+
