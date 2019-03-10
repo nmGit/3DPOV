5e6a2874f5fc600999ff70833da1528611664b15
Noah Meltzer <nmeltzer@wisc.edu>
2019-03-10 14:33:34 -0500

    Platform abstraction layer tools

--- Firmware/3D_POV_Common/PAL/PALThread.cpp
+++ Firmware/3D_POV_Common/PAL/PALThread.cpp
@@ -8,29 +8,54 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include <pthread.h>
+#include <cstring>
 
 
-PALThread::PALThread(unsigned priority, unsigned stack_size)
+PALThread::PALThread(unsigned priority, unsigned stack_size, const char * name)
 {
+#ifdef NAM
     pthread_attr_init(&m_attrs);
     m_priParam.sched_priority = priority;
     m_retc = pthread_attr_setschedparam(&m_attrs, &m_priParam);
-    m_retc |= pthread_attr_setdetatchstate(&attrs, PTHREAD_CREATE_DETATCHED);
+    m_retc |= pthread_attr_setdetachstate(&m_attrs, PTHREAD_CREATE_DETACHED);
     m_retc |= pthread_attr_setstacksize(&m_attrs, stack_size);
+#endif // NAM
+    m_priority = priority;
+    m_stack_size = stack_size;
+    strncpy(m_name, name, PALTHREAD_MAX_NAME_LENGTH);
 }
-
-bool PALThread::Start()
-{
-    m_retc = pthread_create(&m_thread, &m_attrs, Task_handler, NULL);
-}
-
-void PALThread::Task_handler()
+void PALThread::Task_handler(void * arg0)
 {
     uint8_t dummy_data = 0;
-    Task();
-    pthread_exit(&dummy_data);
-    delete this;
+    static_cast<PALThread *>(arg0)->Task();
+    //pthread_exit(&dummy_data);
+    delete arg0;
 }
+bool PALThread::Start()
+{
+
+     unsigned cond_code = xTaskCreate(
+                Task_handler,
+                m_name,
+                m_stack_size,
+                this,
+                m_priority,
+                &m_handle);
+     if(cond_code == pdPASS)
+     {
+         return true;
+     }
+     else
+     {
+         return false;
+     }
+#ifdef NAM
+    m_retc = pthread_create(&m_thread, &m_attrs, Task_handler, NULL);
+#endif //NAM
+
+}
+
+
 
 void PALThread::Task()
 {
--- Firmware/3D_POV_Common/PAL/PALThread.h
+++ Firmware/3D_POV_Common/PAL/PALThread.h
@@ -4,19 +4,25 @@
  *  Created on: Feb 24, 2019
  *      Author: Noah
  */
+#include <FreeRTOS.h>
+#include <task.h>
+#include <pthread.h>
 
 #ifndef SOURCE_PAL_PALTHREAD_H_
 #define SOURCE_PAL_PALTHREAD_H_
 
 
+#define PALTHREAD_MAX_NAME_LENGTH 128
 
-class PALThread()
+class PALThread
 {
 public:
-    PALThread(unsigned priority, unsigned stack_size);
+    PALThread(unsigned priority, unsigned stack_size, const char * name);
     bool Start();
+    static void Task_handler(void * arg0);
+    virtual void Task();
 protected:
-    void Task();
+
 
 private:
     pthread_t           m_thread;
@@ -24,7 +30,11 @@ private:
     struct sched_param  m_priParam;
     int                 m_retc;
 
-    void Task_handler();
-}
+    unsigned            m_priority;
+    unsigned            m_stack_size;
+    TaskHandle_t        m_handle;
+    char                m_name[PALTHREAD_MAX_NAME_LENGTH];
+
+};
 
 #endif /* SOURCE_PAL_PALTHREAD_H_ */
