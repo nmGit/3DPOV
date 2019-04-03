cb25928e1f385cb48f216ccf31663ea06b848430
Noah Meltzer <nmeltzer@wisc.edu>
2019-04-03 15:05:00 -0500

    Update to use PAL facilities rather than direct FreeRTOS facilities

--- Firmware/3D_POV_Main_Board/source/Threads/MotorThread.cpp
+++ Firmware/3D_POV_Main_Board/source/Threads/MotorThread.cpp
@@ -12,9 +12,8 @@
 #include "PAL/PALControl.h"
 #include "DebugUART.h"
 #include "PWM/PWM.h"
-#include "FreeRTOS.h"
 #include "Devices/ESC/ESC.h"
-
+#include "PAL/PALDelay.h"
 MotorThread::MotorThread(unsigned priority, unsigned stack_size, const char * name):
     PALThread(priority, stack_size, name)
 {
@@ -24,32 +23,29 @@ void MotorThread::Task()
 {
     dbg_printf("In Motor Thread\r\n");
 
-    PALYield();
-
     uint16_t var;
     uint16_t pct_pwr = 0;
 
     dbg_printf("\rMotor at %d percent power", pct_pwr);
 
-    vTaskDelay(1000);
+    PALDelay_ms(1000);
     pct_pwr = pct_pwr_esc(pct_pwr);
-    vTaskDelay(5000);
+    PALDelay_ms(5000);
 
     while(1){
         for (var = 0; var < 15; ++var) {
-            vTaskDelay(250);
+            PALDelay_ms(250);
             pct_pwr++;
             pct_pwr_esc(pct_pwr);
             dbg_printf("\rMotor at %d percent power", pct_pwr);
         }
         for (var = 0; var < 15; ++var) {
-            vTaskDelay(250);
+            PALDelay_ms(250);
             pct_pwr--;
             pct_pwr_esc(pct_pwr);
             dbg_printf("\rMotor at %d percent power", pct_pwr);
         }
         dbg_printf("\rMotor at %d percent power", pct_pwr);
-        vTaskDelay(5000);
-        PALYield();
+        PALDelay_ms(5000);
     }
 }
