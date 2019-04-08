98256fb0b9ab37535927d10d4188be23c88ad689
Cassidy <cschneider6@wisc.edu>
2019-04-08 15:09:42 -0500

    created bluetooth uart structure

---
+++ Firmware/3D_POV_Main_Board/source/communication/include/BluetoothUART.h
@@ -0,0 +1,30 @@
+/*
+ * BluetoothUART.h
+ *
+ *  Created on: Apr 2, 2019
+ *      Author: Cassidy
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include "UART/UART.h"
+
+#ifndef SOURCE_COMMUNICATION_INCLUDE_BLUETOOTHUART_H_
+#define SOURCE_COMMUNICATION_INCLUDE_BLUETOOTHUART_H_
+
+#if __cplusplus
+extern "C" {
+#endif
+
+    void bt_uart_cmd(char* cmd, unsigned timeout_ms = 1000);
+    void bt_uart_write(const uint8_t * buf, unsigned len);
+    unsigned bt_uart_read(uint8_t * buf, unsigned len, unsigned timeout_ms = 1000);
+    char get_val_bt(unsigned timeout_ms = 1000);
+    void put_val_bt(uint8_t val);
+
+#if __cplusplus
+}
+#endif
+
+
+#endif /* SOURCE_COMMUNICATION_INCLUDE_BLUETOOTHUART_H_ */
