38bfffe034ddc577e976cd871b04753aa57e1a63
Cassidy <cschneider6@wisc.edu>
2019-04-22 17:03:12 -0500

    fin testing

--- Firmware/3D_POV_Common/SPI/spi.h
+++ Firmware/3D_POV_Common/SPI/spi.h
@@ -9,34 +9,38 @@
 #ifndef __ECE453_POV_SPI_H__
 #define __ECE453_POV_SPI_H__
 #include <stdint.h>
+#include "irq_handlers.h"
+#include "C:\ti\simplelink_msp432p4_sdk_2_40_00_10\source\ti\devices\msp432p4xx\inc\msp432p401r.h"
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <ti/devices/msp432p4xx/inc/msp.h>
+#include "MSP_EXP432P401R.h"
 
 
-uint32_t base;
+uint32_t base;  // SPI base address
 
-// CMS -- Color constants and brightness constants copied from Radio project's led.h
-// Want these for testing things but could delete later!
-//*****************************************************************************
-// Color Constants -- First 8 bits are don't cares, then order is R G B
-//*****************************************************************************
-#define OFF         0x00000000
-#define MAGENTA     0x00FF00FF
-#define RED         0x00FF0000
-#define ORANGE      0x00FF8000
-#define YELLOW      0x00FFFF00
-#define GREEN       0x0000FF00
-#define CYAN        0x0000FFFF
-#define BLUE        0x000000FF
-#define PURPLE      0x007F00FF
-#define WHITE       0x00FFFFFF
-#define LAVENDER    0x00CC99FF
-
-//*****************************************************************************
-// Brightness Constants
-//*****************************************************************************
-#define FULL    0xFF
-#define HALF    0xF0
-#define DIM     0xE1
-#define NONE    0xE0
+//// CMS: color constants & brightness constants copied from led.h for testing
+////*****************************************************************************
+//// Color Constants -- First 8 bits are don't cares, then order is R G B
+////*****************************************************************************
+//#define OFF         0x00000000
+//#define MAGENTA     0x00FF00FF
+//#define RED         0x00FF0000
+//#define ORANGE      0x00FF8000
+//#define YELLOW      0x00FFFF00
+//#define GREEN       0x0000FF00
+//#define CYAN        0x0000FFFF
+//#define BLUE        0x000000FF
+//#define PURPLE      0x007F00FF
+//#define WHITE       0x00FFFFFF
+//#define LAVENDER    0x00CC99FF
+//
+////*****************************************************************************
+//// Brightness Constants
+////*****************************************************************************
+//#define FULL    0xFF
+//#define HALF    0xF0
+//#define DIM     0x1F
+//#define NONE    0xE0
 
 //*****************************************************************************
 // Single LED Data Structure
@@ -56,13 +60,24 @@ typedef struct {
 data_frame_type*** LEDS;
 
 //*****************************************************************************
+// Image data structure
+// Contains information for all LEDs for a single position (out of 100)
+// in the 360 degree sweep of the image.
+//*****************************************************************************
+typedef struct {
+    LEDS img[100];
+} image_type;
+
+
+//*****************************************************************************
 // LED Count Constants
 //*****************************************************************************
-#define LEDS_PER_FIN        16     // TESTING -- ultimate goal is 16 LEDs/fin
-#define FINS                1     // TESTING -- ultimate goal is 12 fins
+#define LEDS_PER_FIN        16      // TESTING -- ultimate goal is 16 LEDs/fin
+#define FINS                1       // TESTING -- ultimate goal is 12 fins
 #define FRAME_BORDER        4
-#define START_FRAME         0x00  // Transmit 4 times for 32 bits of 0
-#define END_FRAME           0xFF  // Transmit 4 times for 32 bits of 1
+#define START_FRAME         0x00    // Transmit 4 times for 32 bits of 0
+#define END_FRAME           0xFF    // Transmit 4 times for 32 bits of 1
+#define TX_PER_LED          4       // 4 SPI transmissions per LED
 
 //*****************************************************************************
 // Function Prototypes
@@ -112,12 +127,6 @@ void spi_transmit_B_type(void);
 void submit_for_tx_A(uint8_t tx_data);
 void submit_for_tx_B(uint8_t tx_data);
 
-//*****************************************************************************
-// General IRQ A and B Handlers -- Called by a specific A or B handler
-// tx_rdy is set to true and the appropriate interrupt flag is cleared.
-//*****************************************************************************
-void EUSCIAX_IRQHandler(void);
-void EUSCIBX_IRQHandler(void);
 
 
 #endif
