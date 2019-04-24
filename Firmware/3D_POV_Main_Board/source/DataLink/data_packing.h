c9c2debe05d6ee1719dad5c9c96a533863c9d3e3
Noah Meltzer <nmeltzer@wisc.edu>
2019-04-24 13:36:41 -0500

    Add data packing for motor commands

---
+++ Firmware/3D_POV_Main_Board/source/DataLink/data_packing.h
@@ -0,0 +1,14 @@
+#ifndef DATA_PACKING_H
+#define DATA_PACKING_H
+
+typedef struct
+{
+    char descriptor[4];
+    uint8_t seq;
+    uint8_t value;
+    uint8_t checksum;
+}mtr_spd_packet_t;
+
+mtr_spd_packet_t mtr_spd_packet;
+
+#endif // DATA_PACKING_H
